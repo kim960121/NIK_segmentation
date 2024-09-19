b13f97504b1f106f88625b87131df841e1351cce
Haeyeon <kim960121@kaist.ac.kr>
2024-09-19 18:31:23 +0900

    GPU-based

--- PDN_gen.py
+++ PDN_gen.py
@@ -1,219 +1,138 @@
 import numpy as np
 import csv
+import pandas as pd
 import matplotlib.pyplot as plt
 import skrf as rf
 from skrf import Network, Frequency
-from numpy.linalg import inv
-from tqdm import tqdm
 import torch
+from tqdm import tqdm
 
-#This function leaves only the predefined ports and renumbering the ports
+def pdn_predefined(n, m, uc_all, map, PDN_name):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
 
-#This function generates a PDN with n rows and m columns
-def pdn_predefined(n,m, uc_all, map, PDN_name):    
-    num_uc=len(uc_all)
-    s5p=[]
-    for i in range(num_uc):
-        if uc_all[i].shape[1]==5:
-            s5p.append(i+1)
-        
-    new_map=[]
-    total_port=0
+    num_uc = len(uc_all)
+    s5p = [i + 1 for i, uc in enumerate(uc_all) if uc.shape[1] == 5]
+    new_map = []
+    total_port = 0
+
+    # Convert uc_all to PyTorch tensors and move to GPU
+    uc_all = [torch.tensor(uc, dtype=torch.complex64).to(device) for uc in uc_all]
 
     for k in tqdm(range(n)):
-        port_num=0
-        print(map[k,:])
+        port_num = 0
+        print(map[k, :])
         for i in tqdm(range(m)):
-            num=int(map[k][i])
-            #once for the first uc of a row
-            if i%m==0:
+            num = int(map[k][i])
+            if i % m == 0:
                 if num in s5p:
-                    merged_z=uc_all[num-1]
+                    merged_z = uc_all[num - 1]
                     new_map.append(num)
-                    current_port=1
-                    port_num+=1
-                
+                    current_port = 1
+                    port_num += 1
                 else:
-                    merged_z=uc_all[num-1]
-                    current_port=0
-                    port_num=0
-                
-               
-            #uc to be merged
-            else:   
+                    merged_z = uc_all[num - 1]
+                    current_port = 0
+                    port_num = 0
+            else:
                 if num in s5p:
-                    uc=uc_all[num-1]
+                    uc = uc_all[num - 1]
                     new_map.append(num)
-                    current_port=1
-  
+                    current_port = 1
                 else:
-                    uc=uc_all[num-1]
-                    current_port=0
+                    uc = uc_all[num - 1]
+                    current_port = 0
 
-             
-                i=i-1  
-                seg1=merged_z[:,0:i+2,0:i+2]  
-                seg2=merged_z[:,0:i+2,i+2:i+3]
-                seg3=merged_z[:,0:i+2,i+3:] 
-                seg4=merged_z[:,i+2:i+3,0:i+2] 
-                seg5=merged_z[:,i+2:i+3,i+2:i+3]
-                seg6=merged_z[:,i+2:i+3,i+3:]
-                seg7=merged_z[:,i+3:,0:i+2] 
-                seg8=merged_z[:,i+3:,i+2:i+3]
-                seg9=merged_z[:,i+3:,i+3:]
-                
-                ## define each z components  
-                ## axis = 2 (row-wise), axis = 1 (column-wise)
-                seg_temp1=np.concatenate([np.array(seg1), np.array(seg3)], axis=2)
-                seg_temp2=np.concatenate([np.array(seg7), np.array(seg9)], axis=2)
-            
-                zaa=np.concatenate([np.array(seg_temp1), np.array(seg_temp2)], axis=1)
-                zbb=uc[:,1:,1:]
-                zpp=seg5
-                zqq=uc[:,0:1,0:1]
-                zap=np.concatenate([np.array(seg2), np.array(seg8)], axis=1)
-                zpa=np.concatenate([np.array(seg4), np.array(seg6)], axis=2)
-                zqb=uc[:,0:1,1:]
-                zbq=uc[:,1:,0:1]
-  
-                
-                
-                ## computation of merged z 
-                Z_AA_temp=np.matmul(zap, inv(zpp+zqq))
-                Z_AA=zaa-np.matmul(Z_AA_temp, zpa)
-                
-                Z_AB_temp=np.matmul(zap, inv(zpp+zqq))
-                Z_AB=np.matmul(Z_AB_temp, zqb)
-                
-                Z_BB_temp=np.matmul(zbq, inv(zpp+zqq))
-                Z_BB=zbb-(np.matmul(Z_BB_temp, zqb))
-            
-                Z_BA_temp=np.matmul(zbq, inv(zpp+zqq))
-                Z_BA=np.matmul(Z_BA_temp, zpa)
-                
-                Z_temp1=np.concatenate([np.array(Z_AA), np.array(Z_AB)], axis=2)
-                Z_temp2=np.concatenate([np.array(Z_BA), np.array(Z_BB)], axis=2)
-                Z_temp=np.concatenate([np.array(Z_temp1), np.array(Z_temp2)], axis=1)
-               
+                i = i - 1
+                # Segment the merged_z tensor
+                seg1, seg2, seg3 = merged_z[:, :i+2, :i+2], merged_z[:, :i+2, i+2:i+3], merged_z[:, :i+2, i+3:]
+                seg4, seg5, seg6 = merged_z[:, i+2:i+3, :i+2], merged_z[:, i+2:i+3, i+2:i+3], merged_z[:, i+2:i+3, i+3:]
+                seg7, seg8, seg9 = merged_z[:, i+3:, :i+2], merged_z[:, i+3:, i+2:i+3], merged_z[:, i+3:, i+3:]
 
-                a=i+2
-                b=2*i+3+port_num
-                c=2*i+6+port_num
-                d=2*i+7+port_num
-                    
-                m1=Z_temp[:, 0:a, 0:a]
-                m2=Z_temp[:, 0:a, a:b]
-                m3=Z_temp[:, 0:a, b:c]
-                m4=Z_temp[:, 0:a, c:d]
-                m5=Z_temp[:, a:b, 0:a]
-                m6=Z_temp[:, a:b, a:b]
-                m7=Z_temp[:, a:b, b:c]
-                m8=Z_temp[:, a:b, c:d]
-                m9=Z_temp[:, b:c, 0:a]
-                m10=Z_temp[:, b:c, a:b]
-                m11=Z_temp[:, b:c, b:c]
-                m12=Z_temp[:, b:c, c:d]
-                m13=Z_temp[:, c:d, 0:a]
-                m14=Z_temp[:, c:d, a:b]
-                m15=Z_temp[:, c:d, b:c]
-                m16=Z_temp[:, c:d, c:d]
-            
-                ## Re-arrange  Port numbering 
-                if current_port==1:
-                    temp1=np.concatenate([np.array(m1), np.array(m3), np.array(m2), np.array(m4)], axis=2)
-                    temp2=np.concatenate([np.array(m9), np.array(m11), np.array(m10), np.array(m12)], axis=2)
-                    temp3=np.concatenate([np.array(m5), np.array(m7), np.array(m6), np.array(m8)], axis=2)
-                    temp4=np.concatenate([np.array(m13), np.array(m15), np.array(m14), np.array(m16)], axis=2)
-                    merged_z=np.concatenate([np.array(temp1), np.array(temp2), np.array(temp3), np.array(temp4)], axis=1)
-                    port_num=port_num+1
-                
-                elif current_port==0:
-                    temp1=np.concatenate([np.array(m1), np.array(m3), np.array(m2)], axis=2)
-                    temp2=np.concatenate([np.array(m9), np.array(m11), np.array(m10)], axis=2)
-                    temp3=np.concatenate([np.array(m5), np.array(m7), np.array(m6)], axis=2)
-                    merged_z=np.concatenate([np.array(temp1), np.array(temp2), np.array(temp3)], axis=1)
+                # Concatenate segments
+                zaa = torch.cat([torch.cat([seg1, seg3], dim=2), torch.cat([seg7, seg9], dim=2)], dim=1)
+                zbb = uc[:, 1:, 1:]
+                zpp = seg5
+                zqq = uc[:, 0:1, 0:1]
+                zap = torch.cat([seg2, seg8], dim=1)
+                zpa = torch.cat([seg4, seg6], dim=2)
+                zqb = uc[:, 0:1, 1:]
+                zbq = uc[:, 1:, 0:1]
 
-        if k==0:
-            Z_up=merged_z
-            total_port=port_num
-            seg1=merged_z[:, 1:1+m, 1:1+m]
-            seg2=merged_z[:, 2+2*m:, 2+2*m:]
-            seg3=merged_z[:, 1:1+m, 2+2*m:]
-            seg4=merged_z[:, 2+2*m:, 1:1+m]
-            temp1=np.concatenate([np.array(seg1), np.array(seg3)], axis=2)
-            temp2=np.concatenate([np.array(seg4), np.array(seg2)], axis=2)
-            Z_up=np.concatenate([np.array(temp1), np.array(temp2)], axis=1)
-            
-        elif k!=0:
-            Z_lo=merged_z
+                # Compute merged z
+                Z_AA_temp = torch.matmul(zap, torch.inverse(zpp + zqq))
+                Z_AA = zaa - torch.matmul(Z_AA_temp, zpa)
+                Z_AB = torch.matmul(Z_AA_temp, zqb)
+                Z_BB_temp = torch.matmul(zbq, torch.inverse(zpp + zqq))
+                Z_BB = zbb - torch.matmul(Z_BB_temp, zqb)
+                Z_BA = torch.matmul(Z_BB_temp, zpa)
 
-            ##upper block Z-segmentation            
-            zpp=Z_up[:, :m, :m]
-            zaa=Z_up[:, m:m+total_port, m:m+total_port]
-            zpa=Z_up[:, :m, m:m+total_port]
-            zap=Z_up[:, m:m+total_port, :m]
-            
-            ## lower block Z-segmentation
-            lo_1=Z_lo[:, 1:1+m, 1:1+m]
-            lo_2=Z_lo[:, 1:1+m, 2+m:2+2*m]
-            lo_3=Z_lo[:, 1:1+m, 2+2*m:2+2*m+port_num]
-            lo_4=Z_lo[:, 2+m:2+2*m, 1:1+m]
-            lo_5=Z_lo[:, 2+m:2+2*m, 2+m:2+2*m]
-            lo_6=Z_lo[:, 2+m:2+2*m, 2+2*m:2+2*m+port_num]
-            lo_7=Z_lo[:, 2+2*m:2+2*m+port_num, 1:1+m]
-            lo_8=Z_lo[:, 2+2*m:2+2*m+port_num, 2+m:2+2*m]
-            lo_9=Z_lo[:, 2+2*m:2+2*m+port_num, 2+2*m:2+2*m+port_num]
-            
-            zbb_temp1=np.concatenate([np.array(lo_1), np.array(lo_3)], axis=2)
-            zbb_temp2=np.concatenate([np.array(lo_7), np.array(lo_9)], axis=2)
-            zbb=np.concatenate([np.array(zbb_temp1), np.array(zbb_temp2)], axis=1)
-            zqq=lo_5
-            zbq=np.concatenate([np.array(lo_2), np.array(lo_8)], axis=1)
-            zqb=np.concatenate([np.array(lo_4), np.array(lo_6)], axis=2)
-              
-            ## computation of merged z    
-            Z_AA_temp=np.matmul(zap, inv(zpp+zqq))
-            Z_AA=zaa-np.matmul(Z_AA_temp, zpa)
-            Z_AB_temp=np.matmul(zap, inv(zpp+zqq))
-            Z_AB=np.matmul(Z_AB_temp, zqb)
-            Z_BB_temp=np.matmul(zbq, inv(zpp+zqq))
-            Z_BB=zbb-(np.matmul(Z_BB_temp, zqb))
-            Z_BA_temp=np.matmul(zbq, inv(zpp+zqq))
-            Z_BA=np.matmul(Z_BA_temp, zpa)
-            Z_temp1=np.concatenate([np.array(Z_AA), np.array(Z_AB)], axis=2)
-            Z_temp2=np.concatenate([np.array(Z_BA), np.array(Z_BB)], axis=2)
-            Z_temp=np.concatenate([np.array(Z_temp1), np.array(Z_temp2)], axis=1)
-            #rearrange in order of m for connection and rest for the ports
-            
-            
-            #total port: ports on Z_up
-            #port_num: ports on
-            Z_1=Z_temp[:, :total_port, :total_port]
-            Z_2=Z_temp[:, :total_port, total_port:total_port+m]
-            Z_3=Z_temp[:, :total_port, total_port+m:total_port+m+port_num]
-            Z_4=Z_temp[:, total_port:total_port+m, :total_port]
-            Z_5=Z_temp[:, total_port:total_port+m, total_port:total_port+m]
-            Z_6=Z_temp[:, total_port:total_port+m, total_port+m:total_port+m+port_num]
-            Z_7=Z_temp[:, total_port+m:total_port+m+port_num, :total_port]
-            Z_8=Z_temp[:, total_port+m:total_port+m+port_num, total_port:total_port+m]
-            Z_9=Z_temp[:, total_port+m:total_port+m+port_num, total_port+m:total_port+m+port_num]
-            Ztemp1=np.concatenate([np.array(Z_5), np.array(Z_4), np.array(Z_6)], axis=2)
-            Ztemp2=np.concatenate([np.array(Z_2), np.array(Z_1), np.array(Z_3)], axis=2)
-            Ztemp3=np.concatenate([np.array(Z_8), np.array(Z_7), np.array(Z_9)], axis=2)
-            Z=np.concatenate([np.array(Ztemp1), np.array(Ztemp2), np.array(Ztemp3)], axis=1)
-            
-            Z_up=Z
-            total_port=total_port+port_num
-            
-        #print(np.shape(Z_up))
-        #print("total_port_num", total_port)
+                Z_temp = torch.cat([torch.cat([Z_AA, Z_AB], dim=2), torch.cat([Z_BA, Z_BB], dim=2)], dim=1)
 
-        
-    num_port=np.size(Z_up,1)-total_port
+                # Re-arrange Port numbering
+                a, b, c, d = i+2, 2*i+3+port_num, 2*i+6+port_num, 2*i+7+port_num
+                m1, m2, m3, m4 = Z_temp[:, :a, :a], Z_temp[:, :a, a:b], Z_temp[:, :a, b:c], Z_temp[:, :a, c:d]
+                m5, m6, m7, m8 = Z_temp[:, a:b, :a], Z_temp[:, a:b, a:b], Z_temp[:, a:b, b:c], Z_temp[:, a:b, c:d]
+                m9, m10, m11, m12 = Z_temp[:, b:c, :a], Z_temp[:, b:c, a:b], Z_temp[:, b:c, b:c], Z_temp[:, b:c, c:d]
+                m13, m14, m15, m16 = Z_temp[:, c:d, :a], Z_temp[:, c:d, a:b], Z_temp[:, c:d, b:c], Z_temp[:, c:d, c:d]
 
-    #remove side ports
-    PDN=Z_up[:,num_port:,num_port:]
-    print("PDN shape", np.shape(PDN))
+                if current_port == 1:
+                    temp1 = torch.cat([m1, m3, m2, m4], dim=2)
+                    temp2 = torch.cat([m9, m11, m10, m12], dim=2)
+                    temp3 = torch.cat([m5, m7, m6, m8], dim=2)
+                    temp4 = torch.cat([m13, m15, m14, m16], dim=2)
+                    merged_z = torch.cat([temp1, temp2, temp3, temp4], dim=1)
+                    port_num += 1
+                elif current_port == 0:
+                    temp1 = torch.cat([m1, m3, m2], dim=2)
+                    temp2 = torch.cat([m9, m11, m10], dim=2)
+                    temp3 = torch.cat([m5, m7, m6], dim=2)
+                    merged_z = torch.cat([temp1, temp2, temp3], dim=1)
+
+        if k == 0:
+            Z_up = merged_z
+            total_port = port_num
+            seg1, seg2 = Z_up[:, 1:1+m, 1:1+m], Z_up[:, 2+2*m:, 2+2*m:]
+            seg3, seg4 = Z_up[:, 1:1+m, 2+2*m:], Z_up[:, 2+2*m:, 1:1+m]
+            Z_up = torch.cat([torch.cat([seg1, seg3], dim=2), torch.cat([seg4, seg2], dim=2)], dim=1)
+        else:
+            Z_lo = merged_z
+
+            zpp = Z_up[:, :m, :m]
+            zaa = Z_up[:, m:m+total_port, m:m+total_port]
+            zpa = Z_up[:, :m, m:m+total_port]
+            zap = Z_up[:, m:m+total_port, :m]
+
+            lo_1, lo_2, lo_3 = Z_lo[:, 1:1+m, 1:1+m], Z_lo[:, 1:1+m, 2+m:2+2*m], Z_lo[:, 1:1+m, 2+2*m:2+2*m+port_num]
+            lo_4, lo_5, lo_6 = Z_lo[:, 2+m:2+2*m, 1:1+m], Z_lo[:, 2+m:2+2*m, 2+m:2+2*m], Z_lo[:, 2+m:2+2*m, 2+2*m:2+2*m+port_num]
+            lo_7, lo_8, lo_9 = Z_lo[:, 2+2*m:2+2*m+port_num, 1:1+m], Z_lo[:, 2+2*m:2+2*m+port_num, 2+m:2+2*m], Z_lo[:, 2+2*m:2+2*m+port_num, 2+2*m:2+2*m+port_num]
+
+            zbb = torch.cat([torch.cat([lo_1, lo_3], dim=2), torch.cat([lo_7, lo_9], dim=2)], dim=1)
+            zqq = lo_5
+            zbq = torch.cat([lo_2, lo_8], dim=1)
+            zqb = torch.cat([lo_4, lo_6], dim=2)
+
+            Z_AA_temp = torch.matmul(zap, torch.inverse(zpp + zqq))
+            Z_AA = zaa - torch.matmul(Z_AA_temp, zpa)
+            Z_AB = torch.matmul(Z_AA_temp, zqb)
+            Z_BB_temp = torch.matmul(zbq, torch.inverse(zpp + zqq))
+            Z_BB = zbb - torch.matmul(Z_BB_temp, zqb)
+            Z_BA = torch.matmul(Z_BB_temp, zpa)
+            Z_temp = torch.cat([torch.cat([Z_AA, Z_AB], dim=2), torch.cat([Z_BA, Z_BB], dim=2)], dim=1)
+
+            Z_1, Z_2, Z_3 = Z_temp[:, :total_port, :total_port], Z_temp[:, :total_port, total_port:total_port+m], Z_temp[:, :total_port, total_port+m:total_port+m+port_num]
+            Z_4, Z_5, Z_6 = Z_temp[:, total_port:total_port+m, :total_port], Z_temp[:, total_port:total_port+m, total_port:total_port+m], Z_temp[:, total_port:total_port+m, total_port+m:total_port+m+port_num]
+            Z_7, Z_8, Z_9 = Z_temp[:, total_port+m:total_port+m+port_num, :total_port], Z_temp[:, total_port+m:total_port+m+port_num, total_port:total_port+m], Z_temp[:, total_port+m:total_port+m+port_num, total_port+m:total_port+m+port_num]
+
+            Z = torch.cat([torch.cat([Z_5, Z_4, Z_6], dim=2), torch.cat([Z_2, Z_1, Z_3], dim=2), torch.cat([Z_8, Z_7, Z_9], dim=2)], dim=1)
+
+            Z_up = Z
+            total_port = total_port + port_num
+
+    num_port = Z_up.size(1) - total_port
+    PDN = Z_up[:, num_port:, num_port:]
+    print("PDN shape", PDN.shape)
     print("new map", new_map)
 
-    return(PDN, new_map)
+    return PDN.cpu().numpy(), new_map
+
